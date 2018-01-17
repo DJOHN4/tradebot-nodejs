2e34e28891d352170ffcbf6349c007042f4f0103
Dinu John <33615369+DJOHN4@users.noreply.github.com>
2018-01-17 13:58:57 -0500

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -1,1 +1,13 @@
 
+
+class ticker {
+    constructor(code, price) {
+        this.stockcode = code;
+        this.price = price;
+    }
+    getticketsymbol(){
+        return this.stockcode;
+    }
+}
+
+module.exports = ticker;
